c3eb4d81972173ae36ebd6e0e14f5afa2fb96646
Rahul Singh <rsnr936313@gmail.com>
2026-05-10 08:38:24 +0000

    last commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,7 +17,7 @@ export const Route = createFileRoute("/")({
   component: Index,
 });
 
-function Index() {
+export function Index() {
   return (
     <div className="relative min-h-screen">
       <Loader />
